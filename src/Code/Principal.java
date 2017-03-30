8168806841ab684b79d25cc157a568d81cb61147
Xoel <xoelveiga@gmail.com>
2017-03-30 10:07:56 +0200

    5º commit
    methods to any button, less =

--- src/Code/Principal.java
+++ src/Code/Principal.java
@@ -1,14 +1,12 @@
 package Code;
 
-import Interface.RealInterface;
-
 public class Principal {
 
 	public static void main(String[] args) {
 		float mun1,num2,result;
 		RealInterface a=new RealInterface();
 		a.main(args);
-		
+
 	}
 
 }
--- src/Code/RealInterface.java
+++ src/Code/RealInterface.java
@@ -1,4 +1,4 @@
-package Interface;
+package Code;
 
 import java.awt.BorderLayout;
 import java.awt.EventQueue;
@@ -12,16 +12,14 @@ import javax.swing.JButton;
 import javax.swing.JFrame;
 import javax.swing.JLabel;
 import javax.swing.JPanel;
-import javax.swing.JTextField;
 
 public class RealInterface {
 
 	private JFrame frame;
-	private JTextField Result;
-	private JTextField Oper;
-	float num1N,num2N,resul;//To set data
-	float[] num1=new float[38];//Data collection
-	float[] num2=new float[38];//Data collection
+	float num1N, num2N, resul;// To set data
+	String num1, num2, oper;// This will be setted by operation
+	String operation="";
+
 	/**
 	 * Launch the application.
 	 */
@@ -29,8 +27,10 @@ public class RealInterface {
 		EventQueue.invokeLater(new Runnable() {
 			public void run() {
 				try {
+
 					RealInterface window = new RealInterface();
 					window.frame.setVisible(true);
+
 				} catch (Exception e) {
 					e.printStackTrace();
 				}
@@ -49,60 +49,100 @@ public class RealInterface {
 	 * Initialize the contents of the frame.
 	 */
 	private void initialize() {
-		//TODO Set all event handler and the things it have to do with var declarated on class
+		// TODO Set all event handler and the things it have to do with var
+		// declarated on class
 		frame = new JFrame();
 		frame.setBounds(100, 100, 450, 300);
 		frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
 		frame.getContentPane().setLayout(new BorderLayout(0, 0));
-		
+
 		JPanel Results = new JPanel();
 		frame.getContentPane().add(Results, BorderLayout.SOUTH);
 		Results.setLayout(new BorderLayout(0, 0));
-		
-		Oper = new JTextField();
-		Results.add(Oper, BorderLayout.NORTH);
-		Oper.setColumns(10);
-		
-		Result = new JTextField();
-		Results.add(Result, BorderLayout.SOUTH);
-		Result.setColumns(10);
-		
+
+		JLabel lblOper = new JLabel();// Operation
+		Results.add(lblOper, BorderLayout.NORTH);
+
+		JLabel lblResul = new JLabel(getResul());// Result
+		Results.add(lblResul, BorderLayout.SOUTH);
+
 		JPanel numOper = new JPanel();
 		frame.getContentPane().add(numOper, BorderLayout.CENTER);
 		numOper.setLayout(new BorderLayout(0, 0));
-		
+
 		JPanel operations = new JPanel();
 		numOper.add(operations, BorderLayout.NORTH);
-		
+
 		JLabel lblNewLabel = new JLabel("  Basic operations  ");
 		operations.add(lblNewLabel);
-		
+
 		JButton btnDiv = new JButton("/");
+		btnDiv.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				operation = operation + "/";
+				lblOper.setText(operation);
+				
+			}
+		});
 		operations.add(btnDiv);
-		
+
 		JButton btnMul = new JButton("*");
+		btnMul.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				operation = operation + "*";
+				lblOper.setText(operation);
+				
+			}
+		});
 		operations.add(btnMul);
-		
+
 		JButton btnSubstraction = new JButton("-");
+		btnSubstraction.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				operation = operation + "-";
+				lblOper.setText(operation);
+			}
+		});
 		operations.add(btnSubstraction);
-		
+
 		JButton btnAdd = new JButton("+");
+		btnAdd.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				operation = operation + "+";
+				lblOper.setText(operation);
+			}
+		});
 		operations.add(btnAdd);
-		
+
 		JButton btnResult = new JButton("=");
+		btnResult.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				// TODO put operation on bot label
+			}
+		});
 		operations.add(btnResult);
-		
+
 		JPanel nums = new JPanel();
 		numOper.add(nums, BorderLayout.CENTER);
 		GridBagLayout gbl_nums = new GridBagLayout();
-		gbl_nums.columnWidths = new int[]{25, 114, 114, 114, 24, 0};
-		gbl_nums.rowHeights = new int[]{0, 30, 29, 30, 30, 0, 0};
-		gbl_nums.columnWeights = new double[]{0.0, 1.0, 1.0, 1.0, 0.0, Double.MIN_VALUE};
-		gbl_nums.rowWeights = new double[]{0.0, 1.0, 1.0, 1.0, 1.0, 0.0, Double.MIN_VALUE};
+		gbl_nums.columnWidths = new int[] { 25, 114, 114, 114, 24, 0 };
+		gbl_nums.rowHeights = new int[] { 0, 30, 29, 30, 30, 0, 0 };
+		gbl_nums.columnWeights = new double[] { 0.0, 1.0, 1.0, 1.0, 0.0, Double.MIN_VALUE };
+		gbl_nums.rowWeights = new double[] { 0.0, 1.0, 1.0, 1.0, 1.0, 0.0, Double.MIN_VALUE };
 		nums.setLayout(gbl_nums);
-		
+
 		JButton btn7 = new JButton("7");
 		btn7.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				operation = operation + "7";
+				lblOper.setText(operation);
+			}
 		});
 		GridBagConstraints gbc_btn7 = new GridBagConstraints();
 		gbc_btn7.fill = GridBagConstraints.BOTH;
@@ -110,88 +150,165 @@ public class RealInterface {
 		gbc_btn7.gridx = 1;
 		gbc_btn7.gridy = 1;
 		nums.add(btn7, gbc_btn7);
-		
+
 		JButton btn8 = new JButton("8");
+		btn8.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				operation = operation + "8";
+				lblOper.setText(operation);
+			}
+		});
 		GridBagConstraints gbc_btn8 = new GridBagConstraints();
 		gbc_btn8.fill = GridBagConstraints.BOTH;
 		gbc_btn8.insets = new Insets(0, 0, 5, 5);
 		gbc_btn8.gridx = 2;
 		gbc_btn8.gridy = 1;
 		nums.add(btn8, gbc_btn8);
-		
+
 		JButton btn9 = new JButton("9");
+		btn9.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				operation = operation + "9";
+				lblOper.setText(operation);
+			}
+		});
 		GridBagConstraints gbc_btn9 = new GridBagConstraints();
 		gbc_btn9.fill = GridBagConstraints.BOTH;
 		gbc_btn9.insets = new Insets(0, 0, 5, 5);
 		gbc_btn9.gridx = 3;
 		gbc_btn9.gridy = 1;
 		nums.add(btn9, gbc_btn9);
-		
+
 		JButton btn4 = new JButton("4");
+		btn4.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent arg0) {
+				operation = operation + "4";
+				lblOper.setText(operation);
+			}
+		});
 		GridBagConstraints gbc_btn4 = new GridBagConstraints();
 		gbc_btn4.fill = GridBagConstraints.BOTH;
 		gbc_btn4.insets = new Insets(0, 0, 5, 5);
 		gbc_btn4.gridx = 1;
 		gbc_btn4.gridy = 2;
 		nums.add(btn4, gbc_btn4);
-		
+
 		JButton btn5 = new JButton("5");
+		btn5.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				operation = operation + "5";
+				lblOper.setText(operation);
+			}
+		});
 		GridBagConstraints gbc_btn5 = new GridBagConstraints();
 		gbc_btn5.fill = GridBagConstraints.BOTH;
 		gbc_btn5.insets = new Insets(0, 0, 5, 5);
 		gbc_btn5.gridx = 2;
 		gbc_btn5.gridy = 2;
 		nums.add(btn5, gbc_btn5);
-		
+
 		JButton btn6 = new JButton("6");
+		btn6.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				operation = operation + "6";
+				lblOper.setText(operation);
+			}
+		});
 		GridBagConstraints gbc_btn6 = new GridBagConstraints();
 		gbc_btn6.fill = GridBagConstraints.BOTH;
 		gbc_btn6.insets = new Insets(0, 0, 5, 5);
 		gbc_btn6.gridx = 3;
 		gbc_btn6.gridy = 2;
 		nums.add(btn6, gbc_btn6);
-		
+
 		JButton btn1 = new JButton("1");
+		btn1.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				operation = operation + "1";
+				lblOper.setText(operation);
+			}
+		});
 		GridBagConstraints gbc_btn1 = new GridBagConstraints();
 		gbc_btn1.fill = GridBagConstraints.BOTH;
 		gbc_btn1.insets = new Insets(0, 0, 5, 5);
 		gbc_btn1.gridx = 1;
 		gbc_btn1.gridy = 3;
 		nums.add(btn1, gbc_btn1);
-		
+
 		JButton btn2 = new JButton("2");
+		btn2.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				operation = operation + "2";
+				lblOper.setText(operation);
+			}
+		});
 		GridBagConstraints gbc_btn2 = new GridBagConstraints();
 		gbc_btn2.fill = GridBagConstraints.BOTH;
 		gbc_btn2.insets = new Insets(0, 0, 5, 5);
 		gbc_btn2.gridx = 2;
 		gbc_btn2.gridy = 3;
 		nums.add(btn2, gbc_btn2);
-		
+
 		JButton btn3 = new JButton("3");
+		btn3.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				operation = operation + "3";
+				lblOper.setText(operation);
+			}
+		});
 		GridBagConstraints gbc_btn3 = new GridBagConstraints();
 		gbc_btn3.insets = new Insets(0, 0, 5, 5);
 		gbc_btn3.fill = GridBagConstraints.BOTH;
 		gbc_btn3.gridx = 3;
 		gbc_btn3.gridy = 3;
 		nums.add(btn3, gbc_btn3);
-		
+
 		JButton btn0 = new JButton("0");
+		btn0.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				operation = operation + "0";
+				lblOper.setText(operation);
+			}
+		});
 		GridBagConstraints gbc_btn0 = new GridBagConstraints();
 		gbc_btn0.fill = GridBagConstraints.BOTH;
 		gbc_btn0.insets = new Insets(0, 0, 5, 5);
 		gbc_btn0.gridx = 1;
 		gbc_btn0.gridy = 4;
 		nums.add(btn0, gbc_btn0);
-		
+
 		JButton btn00 = new JButton("00");
+		btn00.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				operation = operation + "00";
+				lblOper.setText(operation);
+			}
+		});
 		GridBagConstraints gbc_btn00 = new GridBagConstraints();
 		gbc_btn00.fill = GridBagConstraints.BOTH;
 		gbc_btn00.insets = new Insets(0, 0, 5, 5);
 		gbc_btn00.gridx = 2;
 		gbc_btn00.gridy = 4;
 		nums.add(btn00, gbc_btn00);
-		
+
 		JButton btnPoint = new JButton(".");
+		btnPoint.addMouseListener(new MouseAdapter() {
+			@Override
+			public void mouseClicked(MouseEvent e) {
+				operation = operation + ".";
+				lblOper.setText(operation);
+			}
+		});
 		GridBagConstraints gbc_btnPoint = new GridBagConstraints();
 		gbc_btnPoint.insets = new Insets(0, 0, 5, 5);
 		gbc_btnPoint.fill = GridBagConstraints.BOTH;
@@ -199,5 +316,16 @@ public class RealInterface {
 		gbc_btnPoint.gridy = 4;
 		nums.add(btnPoint, gbc_btnPoint);
 	}
+
+	public String getResul() {
+		return Double.toString(doResul(operation));
+
+	}
+	public float doResul(String operation){
+		String[] a=new String[];
+		operation.split("/");
+		return resul;
+	}
+	
 
 }
